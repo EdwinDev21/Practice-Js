f76bb8775a0e16c97fe03e5153c85559090cfb76
Edwin ayerbe <edwinayerbe38@gmail.com>
2024-04-18 19:32:06 +0100

    connect-api

--- advice-generator/script.js
+++ advice-generator/script.js
@@ -1,1 +1,15 @@
-                            
+const API_URl = "https://api.adviceslip.com";
+
+document.getElementById("adviceButoon").addEventListener("click", function () {
+  fetch(`${API_URl}/advice`)
+    .then((res) => res.json())
+    .then((data) => {
+      const advice = data.slip.advice;
+      const titleId = data.slip.id;
+      document.getElementById("paragraph").textContent = advice;
+      document.getElementById("title").textContent = "advice #" + titleId;
+    })
+    .catch((error) => {
+      console.error("Ha ocurrido un error:", error);
+    });
+});
