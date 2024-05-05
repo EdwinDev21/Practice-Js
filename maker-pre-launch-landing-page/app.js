d01306c2fdb2d98b871bb42b7ffd1053a5c9f336
Edwin ayerbe <edwinayerbe38@gmail.com>
2024-05-05 13:08:32 +0100

    add-js

--- maker-pre-launch-landing-page/app.js
+++ maker-pre-launch-landing-page/app.js
@@ -1,1 +1,19 @@
+const input = document.getElementById("input");
+const error = document.getElementById("error");
+const button = document.getElementById("submit-button");
 
+button.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  if (!input.value || !isValidEmail(input.value)) {
+    error.style.display = "inline";
+  } else {
+    error.style.display = "none";
+  }
+});
+
+function isValidEmail(email) {
+  // Utiliza una expresión regular para validar el formato del email
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+}
